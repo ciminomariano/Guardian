55d290cff4f51c2bdad769cab1e605ee4b612901
danieltyukov <60662998+danieltyukov@users.noreply.github.com>
2022-07-05 17:38:10 +0300

    little updates

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from converter_service import ConverterService
-from constants import FILES_IN_HYPERFILE_FORMAT
-
+from Converter.src.constants.constants import FILES_IN_HYPERFILE_FORMAT
+from Converter.src.service.converter_service import ConverterService
 
 if __name__ == '__main__':
     #Instantiate the service
